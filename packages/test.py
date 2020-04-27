bd78aa1a65d286c3405d681fb6e905634023c9de
abdksyed <62547949+abksyed@users.noreply.github.com>
2020-04-27 19:49:51 +0530

    Add files via upload

---
+++ packages/test.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+test_loss = []
+
+test_acc = []
+
+
+def test(model, device, test_loader):
+    model.eval()
+    correct = 0
+    tloss = 0
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+
+            tloss += F.nll_loss(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    tloss /= len(test_loader.dataset)
+    test_loss.append(tloss)
+    print(100 * correct/len(test_loader.dataset))
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+          tloss, correct, len(test_loader.dataset),
+          100 * correct/len(test_loader.dataset)))
+
+    test_acc.append(100 * correct/len(test_loader.dataset))
