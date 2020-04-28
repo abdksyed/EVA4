33d4425583fb450e4d450a35e46c425fbb375891
abdksyed <62547949+abksyed@users.noreply.github.com>
2020-04-28 15:50:00 +0530

    Add files via upload

--- packages/train.py
+++ packages/train.py
@@ -12,7 +12,7 @@ train_acc = []
 train_endacc = []
 
 
-def train(model, device, train_loader, optimizer, epoch):
+def train_nol1(model, device, train_loader, optimizer, epoch):
     model.train()
     pbar = tqdm(train_loader)
     correct = 0
@@ -25,6 +25,41 @@ def train(model, device, train_loader, optimizer, epoch):
         y_pred = model(data)
 
         loss = F.nll_loss(y_pred, target)
+        train_loss.append(loss)
+
+        loss.backward()
+        optimizer.step()
+
+        pred = y_pred.argmax(dim=1, keepdim=True)
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+
+        pbar.set_description(
+            desc=f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+        train_acc.append(100 * correct/processed)
+    train_endacc.append(train_acc[-1])
+
+
+def train_wl1(model, device, train_loader, optimizer, epoch):
+    model.train()
+    pbar = tqdm(train_loader)
+    correct = 0
+    processed = 0
+    for batch_idx, (data, target) in enumerate(pbar):
+        data, target = data.to(device), target.to(device)
+
+        optimizer.zero_grad()
+
+        y_pred = model(data)
+
+        loss = F.nll_loss(y_pred, target)
+        l1_crit = nn.L1loss(size_average=False)
+        reg_loss = 0
+        for param in model.parameters():
+            reg_loss += l1_crit(param)
+        lam = 0.0005
+        loss += lam * reg_loss
+
         train_loss.append(loss)
 
         loss.backward()
