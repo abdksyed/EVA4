1c72c99c3030bd3953b7a73a669bb313909d53cc
abksyed <abdksyed@gmail.com>
2020-05-10 18:09:06 +0530

    Assignment S8

---
+++ S8/packages/summary.py
@@ -0,0 +1,9 @@
+import torch
+import ResNet18 as m
+from torchsummary import summary
+
+def summ(device):
+
+    print(device)
+    model = m.ResNet18().to(device)
+    summary(model, input_size=(3, 32, 32))
